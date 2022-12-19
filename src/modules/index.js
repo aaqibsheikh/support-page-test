b1e5044f269e2bb736b03195efaf4eecdc57c45a
aaqib <aaqibnadeem123@gmail.com>
2022-12-19 18:41:16 +0500

    bug fix

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,6 +1,5 @@
 export { Profile } from './Profile/component.jsx';
 export { Products } from './Products/component.jsx';
 export { KnowledgeBase } from './KnowledgeBase/component';
-export { Tickets } from './Tickets/component.jsx';
 
 export * from './Bills';
